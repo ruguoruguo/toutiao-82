4dc589cfdcd1c029c391bff6798ba25ce4582e74
yanglingyu <3031575573@qq.com>
2019-08-31 14:17:11 +0800

    log

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,31 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+   <child-b >
+     789
+     <template slot-scope="obj">
+       {{obj.name1}}
+     </template>
+     </child-b>
+
   </div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+import childB from './components/child-b'
+export default {
+  name: 'app',
+  data () {
+    return {
+      city: ['北京', '上海', '天津']
     }
+  },
+  components: {
+    'child-b': childB
   }
+
 }
+</script>
+
+<style lang="less">
+
 </style>
---
+++ src/components/child-b.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+     <li >子内容</li>
+     <!-- <slot ></slot> -->
+     <slot :name1="count"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      count: '李四'
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
